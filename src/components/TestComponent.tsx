4308699f5130b6ef0338fc489c921a2b1430cce4
Leekwanyong <dlrkdid12@gmail.com>
2025-04-17 11:42:44 +0900

    asd

---
+++ src/components/TestComponent.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// 함수 선언식 컴포넌트
+const TestComponent = () => {
+  const test = 'Hello World';
+  const a = 1;
+  return <div>{test}</div>;
+};
+
+export default TestComponent;
